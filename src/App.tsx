ded2a7d33bc42496902ba6d98f2f94e4c6cf4500
jankenshow <perchon001@gmail.com>
2021-06-09 16:43:50 +0900

    introduce react hot module replacement

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './css/styles.css'
 import IMAGE from './images/react.png'
 import LOGO from './images/logo.svg'
+import { ClickCounter } from './ClickCounter'
 
 export const App = () => {
     return <>
-        <h1>React Typescript Webpack Starter Template - {process.env.NODE_ENV} {process.env.name}</h1>
+        <h1>Edited React Typescript Webpack Starter Template - {process.env.NODE_ENV} {process.env.name}</h1>
         <img src={IMAGE} alt="React Logo" width="300" height="200" />
         <img src={LOGO} alt="React Logo" width="300" />
+        <ClickCounter />
     </>
 }
