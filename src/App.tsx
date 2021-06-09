1b61ab9a96a2a94c914a1356967ce42ff8e2351c
jankenshow <perchon001@gmail.com>
2021-06-09 15:38:01 +0900

    apply css and images

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import './css/styles.css'
+import IMAGE from './images/react.png'
+import LOGO from './images/logo.svg'
+
 export const App = () => {
-    return <h1>React Typescript Webpack Starter Template</h1>
+    return <>
+        <h1>React Typescript Webpack Starter Template</h1>
+        <img src={IMAGE} alt="React Logo" width="300" height="200" />
+        <img src={LOGO} alt="React Logo" width="300" />
+    </>
 }
